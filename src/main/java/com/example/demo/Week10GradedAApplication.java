781270db85a9a0bbf9794c5a1f5691eb313a5844
Harish CH <cheepiri.shett@hcl.com>
2021-12-12 18:59:50 +0530

    first commit

---
+++ src/main/java/com/example/demo/Week10GradedAApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10GradedAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10GradedAApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
